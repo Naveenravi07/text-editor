ddf00cc96314c3b86c12d88ab3a19b3acc797fa6
Naveenravi07 <gamerkidnav@gmail.com>
2024-06-19 20:16:55 +0530

    actions added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use iced::{widget::text, Sandbox, Settings};
+use iced::{
+    widget::{container, text_editor},
+    Sandbox, Settings, Theme,
+};
 
-#[derive(Debug, Clone, Copy)]
-enum Messages {}
+#[derive(Debug, Clone)]
+enum Messages {
+    Edit(text_editor::Action),
+}
 
-struct Texteditor {}
+struct Texteditor {
+    content: text_editor::Content,
+}
 
 impl Sandbox for Texteditor {
     type Message = Messages;
@@ -11,16 +18,28 @@ impl Sandbox for Texteditor {
     fn title(&self) -> String {
         String::from("Hed")
     }
+
+    fn theme(&self) -> iced::Theme {
+        Theme::Dark
+    }
+
     fn new() -> Self {
-        todo!()
+        Texteditor {
+            content: text_editor::Content::with_text(include_str!("main.rs")),
+        }
     }
 
     fn update(&mut self, message: Self::Message) {
-        match message {};
+        match message {
+            Messages::Edit(action) => self.content.perform(action),
+        };
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        text("Hello world").into()
+        let editor = text_editor(&self.content)
+            .on_action(Messages::Edit)
+            .height(800);
+        container(editor).padding(10).into()
     }
 }
 
