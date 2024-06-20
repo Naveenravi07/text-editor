1bafca9a5c978541beb31d1f0eef1d6340574993
Naveenravi07 <gamerkidnav@gmail.com>
2024-06-20 23:03:32 +0530

    Added some cool icons

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use iced::{
     executor,
     widget::{button, column, container, horizontal_space, row, text, text_editor},
-    Application, Command, Settings, Theme,
+    Application, Command, Element, Font, Theme,
 };
 use rfd::AsyncFileDialog;
 use std::{
-    env, io, path::{ Path, PathBuf}, sync::Arc
+    env, io,
+    path::{Path, PathBuf},
+    sync::Arc,
 };
 
 #[derive(Clone, Debug)]
@@ -103,9 +105,9 @@ impl Application for Texteditor {
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
         let controls = row![
-            button("New").on_press(Messages::New),
-            button("Open").on_press(Messages::Open),
-            button("Save").on_press(Messages::Save)
+            action(new_icon(), Messages::New),
+            action(open_icon(), Messages::Open),
+            action(save_icon(), Messages::Save)
         ]
         .spacing(10);
 
@@ -130,11 +132,32 @@ impl Application for Texteditor {
                 text(formatted)
             };
 
-
             row![status, horizontal_space(), pos]
         };
         container(column![controls, editor, status_bar]).into()
     }
+}
+
+fn action<'a>(el: Element<'a, Messages>, onpress: Messages) -> Element<'a, Messages> {
+    button(container(el).width(30).center_x().center_y())
+        .on_press(onpress)
+        .padding([5, 10])
+        .into()
+}
+fn icon<'a>(codepoint: char) -> Element<'a, Messages> {
+    const ICON_FONT: Font = Font::with_name("editor-icons");
+    text(codepoint).font(ICON_FONT).into()
+}
+
+fn new_icon<'a>() -> Element<'a, Messages> {
+    icon('\u{E800}')
+}
+
+fn save_icon<'a>() -> Element<'a, Messages> {
+    icon('\u{E801}')
+}
+fn open_icon<'a>() -> Element<'a, Messages> {
+    icon('\u{E802}')
 }
 
 fn default_file() -> PathBuf {
@@ -178,9 +201,14 @@ async fn save_file(path: Option<PathBuf>, text: String) -> Result<PathBuf, Error
         .await
         .map_err(|err| Error::IO(err.kind()));
 
-        Ok(path)
+    Ok(path)
 }
 
 fn main() -> iced::Result {
-    Texteditor::run(Settings::default())
+    Texteditor::run(iced::Settings {
+        fonts: vec![include_bytes!("../fonts/editor-icons.ttf")
+            .as_slice()
+            .into()],
+        ..iced::Settings::default()
+    })
 }
